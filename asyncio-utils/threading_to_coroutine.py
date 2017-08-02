12be89b7d3dabec3d6a19e46910f815e44d9e2ca
lujin <jinlu@epiclouds.net>
2017-08-02 09:42:53 +0800

    [update] 更新注释

--- asyncio-utils/threading_to_coroutine.py
+++ asyncio-utils/threading_to_coroutine.py
@@ -5,6 +5,8 @@
 通过将同步阻塞的函数的调用放到线程中执行，从而不会对主线程造成阻塞，
 然后将执行完的结果通过future来传递给EventLoop，这样就可以从future中拿到阻塞函数中的结果了
 
+在asyncio库中有个类似的函数run_in_executor，就是用来执行一个阻塞调用的，默认使用的线程池（ThreadPoolExecutor）
+
 """
 import asyncio
 import threading
@@ -35,7 +37,7 @@ def coroutine(func):
     @functools.wraps(func)
     async def _wrapper():
         future = asyncio.Future()
-        # 没有对异常进行处理,只是简单的将阻塞函数获取到的结果塞到future的结果中
+        # 没有对异常进行处理,只是简单的将阻塞函数获取到的结果塞到future的结果中，这里可以使用线程池替代
         thread = threading.Thread(target=lambda f: f.set_result(func()), args=(future, ))
         thread.start()
         res = await future
